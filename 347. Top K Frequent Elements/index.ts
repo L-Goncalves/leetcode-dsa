7555bfbdf1f89c38ff06a6664340742dd0d2a758
Lucas Goncalves <ldgoncalvs@gmail.com>
2023-06-11 10:30:34 -0300

    making runtime better and using less memory

--- 347. Top K Frequent Elements/index.ts
+++ 347. Top K Frequent Elements/index.ts
@@ -3,7 +3,6 @@ function topKFrequent(nums: number[], k: number): number[] {
     let map = new Map();
     let topKs: number[] = []
 
-
     for (let i = 0; i < nums.length; i++) {
         if(!map.has(nums[i])){
             map.set(nums[i], 1);
@@ -15,26 +14,14 @@ function topKFrequent(nums: number[], k: number): number[] {
 
     let mapValues =  [...map.values()].sort((a, b) => b - a).slice(0, k)
 
-    console.log(mapValues)
     for (const [key, value] of map.entries()) {
-         if (mapValues.find((item) => item === value)) {
+         if (mapValues.includes(value)) {
             topKs.push(key)
-            console.log(key, 'has been added to the array and', value, 'is the highest number')
         } 
-        
-    
-        if(topKs.includes(key)){
-            map.delete(key) 
-            console.log(key, 'has been removed from the Map')
-        }
         if(topKs.length === k){
             return topKs
         }
       }
-    return []
+   
+      throw new Error("Couldn't solve")
 };
-
-
-const data = topKFrequent([5,3,1,1,1,3,73,1], 2)
-
-console.log('response:', data)
