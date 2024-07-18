82a3936874ce8d5922bfeec4182d979cfd32e28f
Lucas Gonçalves <ldgoncalvs@gmail.com>
2024-07-18 07:04:38 -0300

    add: runtime and memory

--- 2037. Minimum Number of Moves to Seat Everyone/index.ts
+++ 2037. Minimum Number of Moves to Seat Everyone/index.ts
@@ -1,4 +1,5 @@
-//
+// Runtime: 97.78%
+// Memory: 35.81%
 // This is a O(nlogn) Solution because we need to sort the arrays, if we didn't I guess it would become a O(n) solution
 function minMovesToSeat(seats: number[], students: number[]): number {
     let moves = 0;
