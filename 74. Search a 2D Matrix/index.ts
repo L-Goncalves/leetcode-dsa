c100a5587455ccc6bf01fd85a0e716032130cec2
Lucas Gonçalves <ldgoncalvs@gmail.com>
2024-08-23 06:55:23 -0300

    update: 74. Search a 2D Matrix

--- 74. Search a 2D Matrix/index.ts
+++ 74. Search a 2D Matrix/index.ts
@@ -1,26 +1,52 @@
+/* 
+
+This is the row ↓ and the second index is the column
+         matrix [0][2]
+
+    we will be acessing 0 row and column 2 which means number 5
+        0  1  2  3
+    0 [[1, 3, 5*, 7],
+    1 [10, 11, 16, 20],
+    2 [23, 30, 34, 60]]
+
+
+*/
 function searchMatrix(matrix: number[][], target: number): boolean {
-    // Convert to 1D, this is awful i know I plan on going back to this and doing in another way, just wanted to practice binary search
-    let flatMatrix = matrix.flat(1);
-    let start = 0;
-    let end = flatMatrix.length - 1;
-    let middleIndex = Math.floor((start + end) / 2);
-   
-
-    while(flatMatrix[middleIndex] !== target && middleIndex != end){
-        if(flatMatrix[middleIndex] > target){
-            end = middleIndex - 1;
-        }
-        if(flatMatrix[middleIndex] < target){
-            start = middleIndex + 1;
-        }
-        middleIndex = Math.floor((start + end) / 2);
-
-     
+    if(matrix.length == 0 || matrix[0].length == 0){
+        return false;
     }
-   
-    if(flatMatrix[middleIndex] != target){
-        return false
+
+    let rows = matrix.length;
+    let columns = matrix[0].length;
+
+    let left = 0;
+    
+    let right = rows * columns - 1;
+
+    
+
+    while (left <= right) {
+
+        // usually happens in binary search
+        const mid = Math.floor((left + right) / 2);
+        // ???
+        const row = Math.floor(mid / columns);
+        const column = mid % columns;
+
+        const mid_value = matrix[row][column];
+ 
+       if (mid_value === target) {
+            return true;
+        } else if (mid_value < target) {
+            
+            left = mid + 1;
+        } else {
+            // if larger means we need to shrink the right;
+            right = mid - 1;
+        }
+
     }
-    return true
+
+    return false;
+
 };
-
